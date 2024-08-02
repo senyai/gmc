290d3266908f6c393ef4f0dab41d98101053c133
Arseniy Terekhin <senyai@gmail.com>
2024-08-02 16:22:20 +0300

    extract `numpy_to_pixmap` as a function

--- gmc/utils/image.py
+++ gmc/utils/image.py
@@ -1,17 +1,36 @@
-from typing import Dict
+from __future__ import annotations
+from typing import TYPE_CHECKING
 from PyQt5.QtGui import QPixmap, QImage
 from PyQt5.QtWidgets import QMessageBox as MB
 from os.path import getmtime
+if TYPE_CHECKING:
+    import numpy.typing as npt
+    from PIL.Image import Image
 
 
-CONVERT: Dict[int, QImage.Format] = {
+CONVERT: dict[int, QImage.Format] = {
     4: QImage.Format.Format_ARGB32,
-    3: QImage.Format.Format_RGB32,
     3: QImage.Format.Format_RGB888,
     1: QImage.Format.Format_Indexed8,
 }
 
-one_image_cache = (None, None, None)
+one_image_cache: tuple[str | None, float | None, QPixmap | None] = (
+    None, None, None)
+
+
+def numpy_to_pixmap(arr_like: npt.ArrayLike | Image) -> QPixmap:
+    import numpy as np
+    arr = np.ascontiguousarray(arr_like)
+    if arr.dtype != np.uint8:
+        max_val = np.max(arr)
+        arr = (arr / (max_val / 255)).astype(np.uint8)
+    num_channels = 1 if arr.ndim <= 2 else arr.shape[2]
+    qformat = CONVERT[num_channels]
+    pointer, read_only_flag = arr.__array_interface__['data']
+    image = QImage(pointer, arr.shape[1], arr.shape[0],
+                   arr.strides[0], qformat)
+    return QPixmap.fromImage(image)
+
 
 def load_pixmap(path: str) -> QPixmap:
     global one_image_cache
@@ -27,25 +46,12 @@ def load_pixmap(path: str) -> QPixmap:
         if pixmap.isNull():
             try:
                 from PIL import Image
-                import numpy as np
             except ImportError as e:
-                raise ValueError("Failed to load `{path}`. And {e}".format(
-                                e=e, path=path))
+                raise ValueError(f"Failed to load `{path}`. And {e}")
             pil_image = Image.open(path)
-            arr = np.array(pil_image)
-            if arr.dtype != np.uint8:
-                max_val = np.max(arr)
-                arr = (arr / (max_val / 255)).astype(np.uint8)
-            num_channels = 1 if len(arr.shape) <= 2 else arr.shape[2]
-            qformat = CONVERT[num_channels]
-            pointer, read_only_flag = arr.__array_interface__['data']
-            image = QImage(pointer, arr.shape[1], arr.shape[0],
-                        arr.strides[0], qformat)
-            pixmap = QPixmap.fromImage(image)
+            pixmap = numpy_to_pixmap(pil_image)
     except (ValueError, FileNotFoundError) as e:
-        MB.warning(
-            None, "Error", "Error while opening {path}:"
-            "\n\n{e}".format(path=path, e=e))
+        MB.warning(None, "Error", f"Error while opening {path}:\n\n{e}")
         return QPixmap()
     one_image_cache = (path, mtime, pixmap)
     return pixmap

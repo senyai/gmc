312302aab2b498b74c1cb410d7e895c8333db145
Arseniy Terekhin <senyai@gmail.com>
2025-12-01 13:44:28 +0300

    test: stub icon loading code in tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,14 @@
 import sys
 from PyQt5 import QtWidgets
+from PyQt5.QtGui import QIcon, QPixmap
+
+from gmc import utils
+
+
+def stub_get_icon(name: str) -> QIcon:
+    return QIcon(QPixmap(16, 16))
+
+
+utils.get_icon = stub_get_icon
 
 qapplication = QtWidgets.QApplication(sys.argv)

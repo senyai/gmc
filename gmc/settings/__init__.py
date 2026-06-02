fcc02061fdf236b6c478d87e1d1ba2eb3d6800ff
Arseniy Terekhin <senyai@gmail.com>
2026-06-02 14:49:52 +0300

    feat: configurable line colors

--- gmc/settings/__init__.py
+++ gmc/settings/__init__.py
@@ -1,9 +1,8 @@
 from __future__ import annotations
-from typing import Any, ClassVar
-from PyQt5.QtCore import QSettings, QByteArray
+from typing import Any, ClassVar, Callable
+from PyQt5.QtCore import QSettings, QByteArray, Qt
 from PyQt5.QtGui import QColor, QFont
 from PyQt5.QtWidgets import QMainWindow
-
 
 default_font_label = QFont("Arial")
 default_font_label.setPointSizeF(10.0)
@@ -16,17 +15,40 @@ class Settings:
     _defaults: ClassVar[dict[str, QColor | QFont | int]] = {
         "bg_1": QColor(0),
         "bg_2": QColor(0xF, 0xF, 0xF),
+        "line_1": QColor(Qt.GlobalColor.white),
+        "line_2": QColor(Qt.GlobalColor.red),
+        "line_sel_1": QColor(Qt.GlobalColor.yellow),
+        "line_sel_2": QColor(Qt.GlobalColor.blue),
         "font_label": default_font_label,
         "click_ms": 250,
+        "line_w": 0,
         # 'schema': str,
     }
-    __slots__ = tuple(_defaults)
+    __slots__ = tuple(_defaults) + ("_callbacks",)
 
     def __init__(self) -> None:
         value = self.value
+        self._callbacks: set[Callable[[], None]] = set()
         # all values that are set here should also be set in `sync`
         for attr, default_value in self._defaults.items():
             setattr(self, attr, value(attr, default=default_value))
+
+    def register(self, callback: Callable[[], None]):
+        """
+        Register callbacks that will be called when settings are changed
+        """
+        self._callbacks.add(callback)
+        return callback
+
+    def unregister(self, callback: Callable[[], None]):
+        self._callbacks.remove(callback)
+
+    def update(self):
+        """
+        Notify all callbacks. Called from Setting dialog
+        """
+        for callback in self._callbacks:
+            callback()
 
     def get_default(self, key: str):
         return self._defaults[key]
